76f4b31e6691ee85e925747bac5a7e83f52a727b
Alexey <bel.alexeyv@gmail.com>
2020-11-20 09:23:20 +0300

    refactored with using Lombok

--- console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
+++ console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
@@ -2,6 +2,7 @@ package academy.learnprogramming.console;
 
 import academy.learnprogramming.Game;
 import academy.learnprogramming.MessageGenerator;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +13,11 @@ import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
+@Slf4j
 @Component
 public class ConsoleNumberGuess {
 
-    // == constants ==
-    public static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
-
     // == fields
-
     private final Game game;
 
     private final MessageGenerator messageGenerator;
